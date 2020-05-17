f833a0dd922dc424396426952f4b361d3ba1678e
Matt Swieboda <mswiebod@gmail.com>
2020-05-17 16:56:51 -0500

    adds react proxy to kemal app, init tab show endpoint

--- src/tabster.cr
+++ src/tabster.cr
@@ -4,4 +4,22 @@ get "/" do |env|
   render "src/views/app.ecr", "src/views/layouts/react.ecr"
 end
 
+before_all "/tabs/:id" do |env|
+  puts "Setting response content type"
+  env.response.content_type = "application/json"
+end
+
+get "/api/tabs/:id" do |env|
+  puts env.response.headers["Content-Type"] # => "application/json"
+
+  id = env.params.url["id"]
+
+  {
+    "id":     id,
+    "title":  "Lipgloss",
+    "artist": "Charli XCX",
+    "tab":    "alskdjflasdjfl asjdflkjsdf",
+  }.to_json
+end
+
 Kemal.run
