4c2f149822b3dc6b05dbb660a41607e03a90cb8b
Matt Swieboda <mswiebod@gmail.com>
2020-05-17 15:51:29 -0500

    adds react

---
+++ react/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="app">
+      <header className="app-header">
+        <p>
+          tabster
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
