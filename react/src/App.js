3f74ac9eeadba537e305636d3a1944bc75ac273e
Matt Swieboda <mswiebod@gmail.com>
2020-05-17 16:27:29 -0500

    adds react-router and scss

--- react/src/App.js
+++ react/src/App.js
@@ -1,14 +1,59 @@
 import React from 'react';
-import './App.css';
+import './App.scss';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  useParams
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="app">
-      <header className="app-header">
-        <p>
-          tabster
-        </p>
-      </header>
+    <Router>
+      <div className="app">
+        <TopNav />
+
+        <Switch>
+          <Route exact path="/">
+            <Tabs />
+          </Route>
+          <Route path="/tabs/:id">
+            <Tab />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+}
+
+function TopNav() {
+  return (
+    <header className="app-header">
+      <Link to="/">tabster</Link>
+    </header>
+  );
+}
+
+function Tabs() {
+  return (
+    <ul>
+      <li>
+        <Link to="/tabs/123">123 - Testing</Link>
+      </li>
+      <li>
+        <Link to="/tabs/lipgloss">Lipgloss - Charlie XCX</Link>
+      </li>
+    </ul>
+  );
+}
+
+function Tab() {
+  let { id } = useParams();
+
+  return (
+    <div>
+      <h3>Tab: {id}</h3>
     </div>
   );
 }
