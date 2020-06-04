5729d60ae0a36a2539f6ab2b7dc3651e4d6902fe
Matt Swieboda <mswiebod@gmail.com>
2020-06-04 18:05:53 -0500

    release React changes for Heroku

--- public/precache-manifest.c67090cb70627e603d8309576588df97.js
+++ public/precache-manifest.c67090cb70627e603d8309576588df97.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "4af92ccf13c5f57dc7426111687a0aba",
+    "revision": "9a3418d87d5dec15429802a3d788f969",
     "url": "/index.html"
   },
   {
-    "revision": "f4f7b427fd198c57fd8d",
+    "revision": "bbfbe2e3e1f60fb78e46",
     "url": "/static/css/main.9440f138.chunk.css"
   },
   {
-    "revision": "4926f847106af365b89c",
-    "url": "/static/js/2.8edc55ec.chunk.js"
+    "revision": "f370b32b95151d994a5a",
+    "url": "/static/js/2.ab56b92e.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.8edc55ec.chunk.js.LICENSE.txt"
+    "url": "/static/js/2.ab56b92e.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "f4f7b427fd198c57fd8d",
-    "url": "/static/js/main.c0130f32.chunk.js"
+    "revision": "bbfbe2e3e1f60fb78e46",
+    "url": "/static/js/main.b44af008.chunk.js"
   },
   {
     "revision": "c6598964c4e61d944a0e",
--- public/service-worker.js
+++ public/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.911ce7090bc3211fb11cd7c9cd7b3606.js"
+  "/precache-manifest.c67090cb70627e603d8309576588df97.js"
 );
 
 self.addEventListener('message', (event) => {
