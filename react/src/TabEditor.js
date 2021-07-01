39eb3bf033a342fc1f589d036c541f1f1f06395b
Matt Swieboda <mswieboda@users.noreply.github.com>
2021-06-30 22:53:53 -0500

    fixes overwrite delete and selection issues

--- react/src/TabEditor.js
+++ react/src/TabEditor.js
@@ -19,28 +19,29 @@ function TabEditor({value, onChange, insertMode}) {
     const lines = value.split('\n');
     const deleting = origin === "+delete";
     const [fromIndex, toIndex] = [getSelectionIndex(from, lines), getSelectionIndex(to, lines)];
-    const selecting = toIndex - fromIndex > 0;
+    const selecting = toIndex - fromIndex > (deleting ? 1 : 0);
     const textDataStr = textData.join('');
     let text = textDataStr === '' ? '\n' : textDataStr;
-
     let overwriting = deleting || !insertMode;
 
+    // TODO: overwriting same char (- with another -, or x for x etc) doesn't move the cursor
     if (overwriting) {
-      let selectedChars = value.substring(fromIndex, toIndex);
-      const allDashes = selectedChars.split('').every(char => char === '-');
-      overwriting = allDashes && text !== '-' && text !== '\n' && value.charAt(toIndex) != '\n';
+      const selectionOrCharAtCursor = value.substring(fromIndex, toIndex);
+      const allDashes = selectionOrCharAtCursor.split('').every(char => char === '-');
+
+      overwriting = !selecting && allDashes && text !== '-' && text !== '\n' && value.charAt(toIndex) !== '\n';
     }
 
+    // TODO: deleting and clearing with same char (- with another -) doesn't move the cursor
     if (deleting) {
-      // TODO: replacing/deleting same char (- with another -) doesn't move the cursor
-      //       figure out how to move the cursor for this case
-      const deleteWithDash = !selecting && ['-', '\n'].indexOf(value.charAt(toIndex - 1)) === -1;
+      const lastChar = value.charAt(toIndex - 1);
+      const deleteWithDash = !selecting && ['-', '\n'].indexOf(lastChar) === -1;
 
       text = !insertMode && deleteWithDash ? '-' : '';
     }
 
-    const newValue = value.substring(0, fromIndex - (overwriting && deleting && selecting ? 1 : 0)) +
-      text + value.substring(toIndex + (overwriting && !deleting && !selecting ? 1 : 0));
+    const newValue = value.substring(0, fromIndex - (overwriting && deleting ? 1 : 0)) +
+      text + value.substring(toIndex + (overwriting && !deleting ? 1 : 0));
 
     onChange(newValue);
   };
