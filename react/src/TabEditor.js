0f5b5d765be84b677f0bcda6d224d19db06374b1
Matt Swieboda <mswieboda@users.noreply.github.com>
2021-06-30 19:57:38 -0500

    fixes text selection logic for insert/overwrite

--- react/src/TabEditor.js
+++ react/src/TabEditor.js
@@ -6,37 +6,44 @@ import './codemirror-tabster.scss';
 import './TabEditor.scss';
 
 function TabEditor({value, onChange, insertMode}) {
+  const getSelectionIndex = (cursor, lines) => {
+    // NOTE: mapping length + 1 to account for ending '\n' which isn't included in `ch`
+    return lines
+      .slice(0, cursor.line)
+      .map(line => line.length + 1)
+      .reduce((a, b) => a + b, cursor.ch);
+  };
+
   const onBeforeChange = (editor, data, _value) => {
-    let { text } = data;
-    const { to, origin } = data;
-    const { line, ch } = to;
+    const { text: textData, from, to, origin } = data;
     const lines = value.split('\n');
     const deleting = origin === "+delete";
-    // NOTE: length + 1 to account for '\n' which isn't included in `ch`
-    const priorToCursorLineLengths = lines.slice(0, line).map(line => line.length + 1);
-    const cursorIndex = priorToCursorLineLengths.reduce((a, b) => a + b, ch);
+    const [fromIndex, toIndex] = [getSelectionIndex(from, lines), getSelectionIndex(to, lines)];
+    const selecting = toIndex - fromIndex > 0;
+    const textDataStr = textData.join('');
+    let text = textDataStr === '' ? '\n' : textDataStr;
 
-    // squash the array to string
-    text = text.join('');
-    // prep for return/backspace
-    text = text === '' ? '\n' : text;
+    let overwriting = deleting || !insertMode;
 
-    // TODO: doesn't work with selections, need to use `from`
-    let nextChar = value.charAt(cursorIndex);
-    let overwrite = deleting || (!insertMode && nextChar === '-' && text !== '-' && text !== '\n');
+    if (overwriting) {
+      let selectedChars = value.substring(fromIndex, toIndex);
+      const allDashes = selectedChars.split('').every(char => char === '-');
+      overwriting = allDashes && text !== '-' && text !== '\n' && value.charAt(toIndex) != '\n';
+    }
 
     if (deleting) {
       // TODO: replacing/deleting same char (- with another -) doesn't move the cursor
       //       figure out how to move the cursor for this case
-      const deleteWithDash = ['-', '\n'].indexOf(value.charAt(cursorIndex - 1)) === -1;
+      const deleteWithDash = !selecting && ['-', '\n'].indexOf(value.charAt(toIndex - 1)) === -1;
+
       text = !insertMode && deleteWithDash ? '-' : '';
     }
 
-    const newValue = value.substring(0, cursorIndex - (deleting ? 1 : 0)) +
-      text + value.substring(cursorIndex + (overwrite && !deleting ? 1 : 0));
+    const newValue = value.substring(0, fromIndex - (overwriting && deleting && selecting ? 1 : 0)) +
+      text + value.substring(toIndex + (overwriting && !deleting && !selecting ? 1 : 0));
 
     onChange(newValue);
-  }
+  };
 
   return (
     <CodeMirror
